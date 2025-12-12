d7a157c83e89e4602b71ae84855e89efadae051c
Felipe Xavier Reis de Jesus <166186256+felipexavier-08@users.noreply.github.com>
2025-12-12 14:56:16 -0300

    Update title in cadastroFuncionarios.php

--- views/cadastroFuncionarios.php
+++ views/cadastroFuncionarios.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro - Guia de Estilo</title>
+    <title>Cadastro de Funcionarios</title>
     <link rel="stylesheet" href="../css/cadastrarFuncionarios.css">
 </head>
 <body>
