cbce854e5024ac438e87c4936264bc658088a459
felipexavier-08 <fxdev08@gmail.com>
2025-12-15 10:41:18 -0300

    candidato alterado

--- views/test.php
+++ views/test.php
@@ -1,4 +1,8 @@
 <?php
+
+    ini_set('display_errors', '0');
+    error_reporting(E_ALL);
+
     require_once __DIR__ . "/../controllers/CandidatoController.php";
     $controller = new CandidatoController();
     $view = $controller->create("POST");
