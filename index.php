c70dcd8da6eb560f4f82e684cbc3f42b752ca04c
felipexavier-08 <fxdev08@gmail.com>
2025-12-15 10:19:09 -0300

    index

--- index.php
+++ index.php
@@ -31,30 +31,6 @@
 
     </div>
 
-
-    <!--<?php/*
-        require_once __DIR__ . "/models/Funcionario.php";
-        require_once __DIR__ . "/repositories/FuncionarioDAO.php";
-        $dao = new FuncionarioDAO();
-        $funcionario = new Funcionario(
-            1,
-            "Felipe",
-            "Xavier",
-            "44444444444",
-            "2025-12-03",
-            "2025-12-04",
-            "71993295049",
-            "novo1234",
-            "novo1234",
-            true,
-            true,
-            "felipe@email.com",
-            "felipe123"
-        );
-        echo("<br>");
-        $dao->funcionarioPorId(1);*/
-    ?>-->
-
 </body>
 
 </html>
